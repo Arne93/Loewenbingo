c7ac90d32f485c9678d380463555729a40388f7d
Lukas Birklein <lbirklei@students.uni-mainz.de>
2017-09-14 00:39:39 +0200

    finally some explosions done on linux, so no guarantee it works perfectly on windows ¯\_(ツ)_/¯

--- Assets/DynamicGrid.cs
+++ Assets/DynamicGrid.cs
@@ -6,41 +6,48 @@ using UnityEngine.UI;
 
 public class DynamicGrid : MonoBehaviour
 {
+	public GameObject Canvas;
     public GameObject MainPanel;
     public GameObject BingoPanel;
 
     public int col, row;
-    
+
 
     //private Random rng = new Random();
-   
-    
+
+
     // Use this for initialization
     void Start()
     {
-       
 
-        RectTransform parent = gameObject.GetComponent<RectTransform>();
-        GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup>();
 
-        grid.cellSize = new Vector2(parent.rect.width / col, parent.rect.height / row);
+       // RectTransform parent = gameObject.GetComponent<RectTransform>();
+       // GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup>();
+		GridLayoutGroup grid = BingoPanel.GetComponent<GridLayoutGroup> ();
+		RectTransform parent = Canvas.GetComponent<RectTransform> ();
+		Debug.Log (parent.transform.localScale);
+		Debug.Log (parent.rect.width / col);
+
+		grid.cellSize = new Vector2(parent.rect.width / col, parent.rect.height / row);
+
+	//	grid.cellSize = new Vector2(10.333f,10.333f);
+
 
         //initialize the buttons
-        
-        
-        
 
-        
     }
 
-   
 
 
-    
+
+
 
     // Update is called once per frame
     void Update()
     {
+		/* needed if the screen aspect ratio changes, should not happen normally */
+		GridLayoutGroup grid = BingoPanel.GetComponent<GridLayoutGroup> ();
+		grid.cellSize = new Vector2(Screen.width/(float)col, Screen.height/(float)row);
         if (Application.platform == RuntimePlatform.Android)
         {
             if (Input.GetKey(KeyCode.Escape))
@@ -54,8 +61,10 @@ public class DynamicGrid : MonoBehaviour
                 return;
             }
         }
-        
-        if(Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.LinuxPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+
+        if(Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.LinuxPlayer 
+			|| Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.LinuxEditor 
+			|| Application.platform == RuntimePlatform.LinuxPlayer)
         {
             if (Input.GetKeyDown(KeyCode.R))
             {
